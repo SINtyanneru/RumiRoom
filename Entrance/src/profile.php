be752d1868f7c6e5e5bac675f05bcc370762a221
SINtyanneru <rumisan@rumi-room.net>
2026-05-27 22:57:34 +0900

    推し追加

--- Entrance/src/profile.php
+++ Entrance/src/profile.php
@@ -91,6 +91,11 @@ $love_kyara = [
 		"NAME" => "卯月",
 		"ARTIFACT" => "艦隊これくしょん",
 		"URL" => "https://www.pixiv.net/tags/%E5%8D%AF%E6%9C%88(%E8%89%A6%E9%9A%8A%E3%81%93%E3%82%8C%E3%81%8F%E3%81%97%E3%82%87%E3%82%93)/artworks"
+	],
+	[
+		"NAME" => "蓮希るい",
+		"ARTIFACT" => "VTuber",
+		"URL" => "https://www.pixiv.net/tags/%E8%93%AE%E5%B8%8C%E3%82%8B%E3%81%84/artworks"
 	]
 ];
 ?>
